a75f90a689a35ce882cecc31da150def55bed9e8
Aditi Mittal <75011001+Aditi12-art@users.noreply.github.com>
2023-07-13 23:05:18 +0530

    Delete src directory

--- src/shared/Subtitle.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './subtitle.css';
-const Subtitle=({subtitle})=>{
-    return (
-    <h3 className="section_subtitle">{subtitle}</h3>
-    )
-}
-export default Subtitle;
